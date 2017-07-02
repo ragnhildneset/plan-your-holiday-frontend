90f8d8139168e2d8dcf0a1b825ed412247aafc1b
Ragnhild Neset <ragcecnes@gmail.com>
2017-07-02 20:58:09 +0200

    basic styling

--- src/components/enter-journey/enter-journey.js
+++ src/components/enter-journey/enter-journey.js
@@ -4,6 +4,7 @@ import angular from 'angular';
 
 import EnterJourneyComponent from './enter-journey.component';
 
+import './enter-journey.style.css';
 
 export default angular.module('enterJourney', [])
     .component(EnterJourneyComponent.name, new EnterJourneyComponent);
