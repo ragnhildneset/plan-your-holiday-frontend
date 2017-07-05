89df2311aa5b9c251fee3d6952ef2229e01c3953
Julian Frattini <julian.frattini@alice-dsl.net>
2017-07-06 01:43:37 +0200

    Added Must-See selection
    added basic system of the must-see system

--- src/components/category-selection/category-selection.component.js
+++ src/components/category-selection/category-selection.component.js
@@ -19,17 +19,42 @@ class CategorySelectionComponent {
     }
 }
 
-class CategorySelectionComponentController{
+class CategorySelectionComponentController {
+
     constructor($state){
         this.$state = $state;
         this.AttractionsService = AttractionsService;
+        this.selection = [];
+    }
+
+    // simulates a click on a certain box and adds/removes it to/from the list of selected Must-Sees
+    click(index) {
+      //console.log('Clicked ' + index);
+
+      if(this.isContained(index) == -1) {
+        this.selection[this.selection.length] = index;
+      }
+      else {
+        this.selection.splice(this.isContained(index), 1);
+      }
+
+
+      //console.log(this.selection.toString());
+    }
+
+    // calculates whether a given index is already contained in the field selection
+    isContained(index) {
+      for(var i = 0; i < this.selection.length; i++) {
+        if(this.selection[i] == index) {
+          return i;
+        }
+      }
+      return -1;
     }
 
     static get $inject(){
         return ['$state', AttractionsService.name];
     }
-
-
 }
 
 
