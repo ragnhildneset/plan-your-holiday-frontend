5bddcb736d2deb4ad2a3bcf99dc6c5c16134bb8d
Duygu Akdemir <>
2017-07-15 12:04:47 +0200

    created footer

--- src/components/app-content/app-content.js
+++ src/components/app-content/app-content.js
@@ -3,9 +3,11 @@
 import angular from 'angular';
 
 import AppHeader from './../app-header/app-header';
+import AppFooter from './../app-footer/app-footer';
 import AppContentComponent from './app-content.component';
 
 
 export default angular.module('AppView', [
-    AppHeader.name])
+    AppHeader.name,
+    AppFooter.name])
     .component(AppContentComponent.name, new AppContentComponent);
